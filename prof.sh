dcdb766dc9556e0a1908af1fc5bc2205828739ab
shengyushen <shengyushen@aliyun.com>
2019-06-06 02:30:44 -0400

    adding new script

--- prof.sh
+++ prof.sh
@@ -1,1 +1,10 @@
-nvprof --print-gpu-trace --print-nvlink-topology -o %h-%p.nvvp --csv --log-file %h-%p.log "$@"
+#!/bin/bash
+if [ $# -ne 7 ] ; 
+  then echo "Usage : prof.sh <network type> <repeat time> <batch size> <resolution> <> <> <>"
+else 
+  rm -rf ssy_$1_$2_$3_$4_$5_$6_$7*
+  mkdir ssy_$1_$2_$3_$4_$5_$6_$7.out
+  nvprof -f --quiet --profile-api-trace none --print-gpu-trace --print-nvlink-topology --csv --log-file ssy_$1_$2_$3_$4_$5_$6_$7.csv -o ssy_$1_$2_$3_$4_$5_$6_$7.nvvp  ./cudnnModelParallel.exe "$@" 
+fi
+
+
---
+++ runprofall.sh
@@ -0,0 +1,8 @@
+# these are advise version
+                                                   
+./prof.sh  resnet 100 64  448 0 1 0
+./prof.sh  resnet 100 64  448 1 1 0
+./prof.sh  resnet 100 128 224 0 1 0
+./prof.sh  resnet 100 128 224 1 1 0
+                                                   
+
